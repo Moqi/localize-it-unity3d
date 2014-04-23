92cc07351ab1e561e407b18860d6b33eaaf9c4de
Alexey Malorossiianov <funnycrabs@gmail.com>
2014-04-23 23:56:42 +0700

    resolve #2 make base structure, create LocalizeItStorage

---
+++ Assets/example/scripts/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using LocalizeIt;
+
+public class EntryPoint : MonoBehaviour
+{
+    void Awake ()
+    {
+        var localize = new LocalizeItStorage ();
+      
+        var manifest = Resources.Load<TextAsset> ("localize_it/manifest").text;
+        var locales = SimpleJSON.JSON.Parse (manifest) ["locales"].AsArray;
+        string path = "localize_it/{0}/locale";
+        for (int i = 0; i < locales.Count; i++) {
+            var localeId = locales [i].Value;
+            var localeSource = Resources.Load<TextAsset> (string.Format (path, localeId)).text;
+            localize.AddLocaleSource (localeSource, localeId);
+        }
+
+        localize.SetLocale ("en-US");
+        Debug.Log (localize.Get("button_name"));
+        localize.SetLocale ("ru-RU");
+        Debug.Log (localize.Get("button_name"));
+    }
+}
---
+++ Assets/scripts/LocalizeItStorage.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace LocalizeIt
+{
+    public class LocalizeItStorage
+    {
+        private string mCurrentLocaleId;
+        Dictionary<string, SimpleJSON.JSONNode> mLocales = new Dictionary<string, SimpleJSON.JSONNode> ();
+
+        public string Get(string _id)
+        {
+            if (Storage[_id] != null)
+                return Storage [_id].Value;
+
+            Debug.LogWarning (string.Format("LocalizeIt: Undefined value for key: {0}", _id));
+            return "";
+        }
+
+        public void AddLocaleSource(string _source, string _localeId)
+        {
+            var source = SimpleJSON.JSON.Parse (_source);
+            mLocales.Add (_localeId, source);
+
+        }
+
+        public void SetLocale(string _localeId)
+        {
+            if (!mLocales.ContainsKey (_localeId))
+                throw new KeyNotFoundException (string.Format("LocalizeId: Undefined localeId: {0}", _localeId));
+            mCurrentLocaleId = _localeId;
+            Storage = mLocales [_localeId];
+        }
+
+        SimpleJSON.JSONNode Storage;
+    }
+}
